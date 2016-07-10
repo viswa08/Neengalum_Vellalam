5b134583a970117e154b9d24e6e072c93cd005e7
viswa08 <viswa0894@gmail.com>
2016-07-10 17:18:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viswanath/neengalumvellalamorukodi/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.viswanath.neengalumvellalamorukodi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onClick(){
+        Intent i = new Intent(this, question_two.class);
+        startActivity(i);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/viswanath/neengalumvellalamorukodi/question_two.java
@@ -0,0 +1,13 @@
+package com.example.viswanath.neengalumvellalamorukodi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class question_two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_two);
+    }
+}
